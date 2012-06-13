8eae1ea4336dccc0c894432ae5a058f1745751c9
SMam <mamiya.shn@gmail.com>
2012-06-13 10:02:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audioserver::Application.config.secret_token = 'f063f1effa4fe0b5eede38fd1a034457841cebbae80172ee74559d1a1f4ae1331fb5dab5c5a0e19af9262fbfe95fe9010eee736a5f422b1cb04a7fa6cbd5c1d0'
