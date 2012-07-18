cc8d8628bcd621a6e0d6a26d9d41782d23aba59c
SMam <mamiya.shn@gmail.com>
2012-07-18 16:34:26 +0900

    helpers/application_helper_spec.rbを作成し、all greenに / views/patients/show.html.erb_spec.rbbを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  include AudiogramsHelper
 end
--- spec/views/patients/show.html.erb_spec.rb
+++ spec/views/patients/show.html.erb_spec.rb
@@ -1,15 +1,67 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe "patients/show" do
   before(:each) do
-    @patient = assign(:patient, stub_model(Patient,
-      :hp_id => "Hp"
-    ))
+#    @patient = assign(:patient, stub_model(Patient,
+#      :hp_id => "Hp"
+#    ))
+    @patient = stub_model(Patient,
+      :hp_id => "19"
+    )
+    @examdate = Time.now
+    @audiogram_stub = stub_model(Audiogram,
+      :examdate => @examdate,
+      :comment => "Comment",
+      :image_location => "graphs_some_directory",
+      :ac_rt_500 => 10, :ac_rt_1k => 20, :ac_rt_2k => 30,
+      :ac_lt_500 => 15, :ac_lt_1k => 25, :ac_lt_2k => 35,
+      :audiometer => "Audiometer",
+      :hospital => "Hospital"
+      )
   end
 
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Hp/)
+  context "patientに属するデータがない場合" do
+    before do
+      @patient.audiograms = []
+      assign(:patient, @patient)
+    end
+
+    it "renders attributes in <p>" do
+      render
+      # Run the generator again with the --webrat flag if you want to use webrat matchers
+      rendered.should match(Regexp.new(reg_id(@patient.hp_id)))
+                                                      # hp_idがxxx-xxxx-xx-xで表示されること
+      rendered.should match(/No Otolaryngological data/)
+      rendered.should_not match(/Audiograms/)
+    end
+  end
+
+  context "Audiogramがある場合" do
+    before do
+      @patient.audiograms << @audiogram_stub
+      @patient.audiograms << @audiogram_stub
+      @patient.audiograms << @audiogram_stub
+      assign(:patient, @patient)
+    end
+
+    it "renders attributes in <p>" do
+      render
+      # Run the generator again with the --webrat flag if you want to use webrat matchers
+      rendered.should match(Regexp.new(reg_id(@patient.hp_id)))
+                                                      # hp_idがxxx-xxxx-xx-xで表示されること
+      rendered.should_not match(/No Otolaryngological data/)
+      rendered.should match(/Audiograms/)
+      # 3回分のAudiogramを表示
+      assert_select "tr>td", :text => Regexp.new("#{@examdate.strftime("%Y/%m/%d")}"),\
+        :count => 3
+      assert_select "tr>td", :text => Regexp.new("R:.+#{mean("4R", @audiogram_stub)[:R]}"),\
+        :count => 3
+      assert_select "tr>td", :text => Regexp.new("L:.+#{mean("4R", @audiogram_stub)[:L]}"),\
+        :count => 3
+      assert_select "tr>td>a>img", :count => 3
+      thumb_location = "assets/#{@audiogram_stub.image_location.sub("graphs", "thumbnails")}"
+      rendered.should =~ Regexp.new("#{thumb_location}.+#{thumb_location}", Regexp::MULTILINE)
+    end
   end
 end
